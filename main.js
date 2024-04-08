fd285481907091ecb07835023875f967226eacfb
New Hickory <andrew.r.spear@gmail.com>
2024-04-08 17:07:58 -0500

    completed game logic and passed all tests

--- main.js
+++ main.js
@@ -40,97 +40,156 @@ const movePiece = (startStack, endStack) => {
 const isLegal = (startStack, endStack) => {
   // Your code here
 
-  if (startStack == 'a' || startStack == 'b' || startStack == 'c') {
-    if (endStack == 'a' || endStack == 'b' || endStack == 'c') {
-      
-      let startStackIndex = stacks[startStack].length - 1;
-      let moveRing = stacks[startStack][startStackIndex];
-      console.log('startStack:', startStack);
-      console.log('startStackIndex:', startStackIndex);
-      console.log('moveRing:', moveRing);
-      let endStackIndex = stacks[endStack].length;
-      let stackedRing = stacks[endStack][endStackIndex];
-      console.log('stackedRing:', stackedRing)
+  // Initiate result variable as true, return this variable at end of function if all tests are true.
 
-      if (moveRing > stackedRing) {
-        console.log('Move not allowed.  You can only stack smaller rings on larger rings.')
-      }
-      else {
-      }
-    }
-    else {
-      console.log('Enter "a" or "b" or "c" to select a tower to move a ring to.')
-    }
+  let result = true;
+
+  // Check if first input is a valid stack
+  if (startStack == 'a' || startStack == 'b' || startStack == 'c') {
+    result = true;
+  }
+  else {
+    console.log('Enter "a" or "b" or "c" to select a tower to move a ring to.')
+    return false;
+  }
+
+  if (endStack == 'a' || endStack == 'b' || endStack == 'c') {
+    result = true;
   }
   else {
     console.log('Enter "a" or "b" or "c" to select a tower to move a ring from.')
+    return false;
   }
+
+      
+  let startStackIndex = stacks[startStack].length - 1;
+  let moveRing = stacks[startStack][startStackIndex];
+  console.log('startStack:', startStack);
+  console.log('startStackIndex:', startStackIndex);
+  console.log('moveRing:', moveRing);
+  let endStackIndex = stacks[endStack].length - 1;
+  let stackRing = stacks[endStack][endStackIndex];
+  console.log('endStack:', endStack);
+  console.log('endStackIndex:', endStackIndex);
+  console.log('stackRing:', stackRing)
+
+  if (endStackIndex == -1 || moveRing < stackRing) {
+    result = true;
+  }
+  else {
+    console.log('Move not allowed.  You can only stack smaller rings on larger rings.');
+    return false;
+  }
+
+  return result;
+
 }
+
+  // Function to compare arrays
+  const arraysEqual = (arr1, arr2) => {
+    // Check if the arrays have the same length
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    
+    // Check if all elements are equal
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+  }
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
   // Your code here
 
+  let winning_tower = [4, 3, 2, 1];
+
+  if (arraysEqual(stacks['b'], winning_tower) || arraysEqual(stacks['c'], winning_tower)) {
+    return true;
+  }
+  else {
+    console.log('No winner in stack b:', stacks['b']);
+    console.log('No winner in stack c:', stacks['c']);
+    return false;
+  }
 }
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
   // Your code here
-  isLegal(startStack, endStack);
+
+  if (isLegal(startStack, endStack) == true) {
+    console.log('Move is legal.');
+  }
+  else {
+    console.log('Try another move.')
+    return;
+  }
+  
   movePiece(startStack, endStack);
-  checkForWin();
+  
+  if (checkForWin() == true) {
+    console.log('You win in!');
+    return;
+  }
 }
 
-const getPrompt = () => {
-  printStacks();
-  rl.question('start stack: ', (startStack) => {
-    rl.question('end stack: ', (endStack) => {
-      towersOfHanoi(startStack, endStack);
-      getPrompt();
+
+  const getPrompt = () => {
+    printStacks();
+    rl.question('start stack: ', (startStack) => {
+      rl.question('end stack: ', (endStack) => {
+        towersOfHanoi(startStack, endStack);
+        getPrompt();
+      });
     });
-  });
-}
+  }
 
-// Tests
+  // Tests
 
-if (typeof describe === 'function') {
+  if (typeof describe === 'function') {
 
-  describe('#towersOfHanoi()', () => {
-    it('should be able to move a block', () => {
-      towersOfHanoi('a', 'b');
-      assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
+    describe('#towersOfHanoi()', () => {
+      it('should be able to move a block', () => {
+        towersOfHanoi('a', 'b');
+        assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
+      });
     });
-  });
 
-  describe('#isLegal()', () => {
-    it('should not allow an illegal move', () => {
-      stacks = {
-        a: [4, 3, 2],
-        b: [1],
-        c: []
-      };
-      assert.equal(isLegal('a', 'b'), false);
+    describe('#isLegal()', () => {
+      it('should not allow an illegal move', () => {
+        stacks = {
+          a: [4, 3, 2],
+          b: [1],
+          c: []
+        };
+        assert.equal(isLegal('a', 'b'), false);
+      });
+      it('should allow a legal move', () => {
+        stacks = {
+          a: [4, 3, 2, 1],
+          b: [],
+          c: []
+        };
+        assert.equal(isLegal('a', 'c'), true);
+      });
     });
-    it('should allow a legal move', () => {
-      stacks = {
-        a: [4, 3, 2, 1],
-        b: [],
-        c: []
-      };
-      assert.equal(isLegal('a', 'c'), true);
+    describe('#checkForWin()', () => {
+      it('should detect a win', () => {
+        stacks = { a: [], b: [4, 3, 2, 1], c: [] };
+        assert.equal(checkForWin(), true);
+        stacks = { a: [1], b: [4, 3, 2], c: [] };
+        assert.equal(checkForWin(), false);
+      });
     });
-  });
-  describe('#checkForWin()', () => {
-    it('should detect a win', () => {
-      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
-      assert.equal(checkForWin(), true);
-      stacks = { a: [1], b: [4, 3, 2], c: [] };
-      assert.equal(checkForWin(), false);
-    });
-  });
 
-} else {
+  } else {
 
-  getPrompt();
+    getPrompt();
 
-}
+  }
+
